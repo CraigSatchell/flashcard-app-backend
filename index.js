bae643f22881d3da444c3a7cd7696251e62c26c1
Craig Satchell <satchdev@gmail.com>
2021-06-29 12:24:39 -0400

    compl. server test

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+
+app.get('/', (res, req) => {
+    req.send('Hello Fam!');
+})
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
