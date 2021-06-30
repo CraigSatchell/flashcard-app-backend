8a24a6441fc45b7baf6ceac08cad5d9cbf131d9f
Craig Satchell <satchdev@gmail.com>
2021-06-30 01:15:07 -0400

    add cardDeckSchema

--- models/flashcard.js
+++ models/flashcard.js
@@ -1,2 +1,25 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
+
+const cardDeckSchema = new mongoose.Schema({
+    name: { type: String, required: true, minlength: 2, maxlength: 255 },
+    description: { type: String, required: true },
+    cards: { type: Array },
+    dateModified: { type: Date, default: Date.now },
+});
+
+const CardDeck = mongoose.model('CardDeck', cardDeckSchema);
+
+function validateCardDeck(cardDeck) {
+    const schema = Joi.object({
+        name: Joi.string().min(2).max(50).required(),
+        description: Joi.string().required(),
+        cards: Joi.array(),
+    });
+    return schema.validate(cardDeck);
+}
+
+
+exports.CardDeck = CardDeck;
+exports.validateCardDeck = validateCardDeck;
+exports.cardDeckSchema = cardDeckSchema;
