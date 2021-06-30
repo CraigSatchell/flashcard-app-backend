c2ad4f6e66d05675abee62e42eb80ff2892d8cb8
Craig Satchell <satchdev@gmail.com>
2021-06-30 01:56:05 -0400

    add cardDeck GET routes

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const connectDB = require('./startup/db');
 const express = require('express');
 const app = express();
+const carddecks = require('./routes/collections')
 
 connectDB();
+
+app.use(express.json());
+app.use('/api/carddecks', carddecks);
+
+
 
 app.get('/', (res, req) => {
     req.send('Hello Fam!');
--- routes/collections.js
+++ routes/collections.js
@@ -1,3 +1,29 @@
-const {CardDeck, FlashCard, validateFlashCard, validateCardDeck }= require('../models/flashcard')
+const { CardDeck, FlashCard, validateFlashCard, validateCardDeck } = require('../models/flashcard')
 const express = require('express');
 const router = express.Router();
+
+// Get all card collections
+router.get('/', async (req, res) => {
+    try {
+        const cardDecks = await CardDeck.find();
+        return res.send(cardDecks);
+    } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+// Get card collection by id
+router.get('/:id', async (req, res) => {
+    try {
+        const cardDeck = await CardDeck.findById(req.params.id);
+        if (!cardDeck)
+            return res.status(400).send(`The product with id "${req.params.id}" d
+   oes not exist.`);
+        return res.send(cardDeck);
+    } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+
+module.exports = router;
