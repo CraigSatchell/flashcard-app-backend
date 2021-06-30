d74643640458ed3e5529c5879bdc326160bdcb12
Craig Satchell <satchdev@gmail.com>
2021-06-29 23:06:21 -0400

    add startup, routes, models scaffolding

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+const connectDB = require('./startup/db');
 const express = require('express');
 const app = express();
 
+connectDB();
 
 app.get('/', (res, req) => {
     req.send('Hello Fam!');
 })
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
